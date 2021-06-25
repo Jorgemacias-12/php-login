6b0088940d6c2657b3a76b711916430002aae94d
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-06-25 18:21:56 -0500

    Preparativos para empezar las diferentes paginas
    Se ha empezado a crear los archivos de la ruta de registro de la pagina,
    se incluira al repo hasta al ultimo los scripts php encargados del inicio de sesion.
    de momento se van a hacer pruebas.

---
+++ routes/signup.php
@@ -0,0 +1,20 @@
+<?php
+include_once("");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Company - SignUp</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../styles/signup.css'>
+</head>
+
+<body>
+
+</body>
+
+</html>
